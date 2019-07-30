a70b84f9751e05ea274c82c496c6e3b19fb62e4a
Nathan Schapka <ntschapk@mtu.edu>
2019-07-30 12:38:56 -0500

    inputReader reads text from files, refactored tests to not leave open files if test fails

--- WordSearchTests.py
+++ WordSearchTests.py
@@ -8,10 +8,22 @@ class WordSearchTests(unittest.TestCase):
         self.assertEqual(testReader.filePath, 'C://users/nschapka/desktop/input.txt')
 
     def testInputReaderFindsFile(self):
-        testReader = inputReader('C://users/nschapka/desktop/input.txt')
-        fileObject = open(testReader.filePath)
-        self.assertTrue(fileObject)
-        fileObject.close()
+        try:
+            testReader = inputReader('C://users/nschapka/desktop/input.txt')
+            fileObject = open(testReader.filePath)
+            self.assertTrue(fileObject)
+        finally:
+            fileObject.close()
+
+    def testInputReaderReadsTextAndInputNotBlank(self):
+        try:
+            testReader = inputReader('C://users/nschapka/desktop/input.txt')
+            fileObject = open(testReader.filePath)
+            inputText  = testReader.readText(fileObject)
+            self.assertTrue(inputText)  # at this point i just want to check that there is text read - don't care if the input is valid
+        finally:
+            fileObject.close()
+
 
 if __name__ == '__main__':
     unittest.main()
--- inputReader.py
+++ inputReader.py
@@ -2,3 +2,10 @@ class inputReader:
 
     def __init__(self, filePath):
         self.filePath = filePath
+
+    def readText(self, file):
+        self.inputText = []
+        for line in file:
+            self.inputText.append(line)
+
+        return self.inputText
