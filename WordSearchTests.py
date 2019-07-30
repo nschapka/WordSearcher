d2ebff8d330acfc413993172cb2eb7af5fec1117
Nathan Schapka <ntschapk@mtu.edu>
2019-07-30 12:57:22 -0500

    added inputParser class, object is able to accept input handed off from inputReader

--- WordSearchTests.py
+++ WordSearchTests.py
@@ -1,7 +1,12 @@
 import unittest
 from inputReader import inputReader
+from inputParser import inputParser
 
 class WordSearchTests(unittest.TestCase):
+
+    # -----------
+    # inputReader
+    # -----------
 
     def testInputReaderInit(self):
         testReader = inputReader('C://users/nschapka/desktop/input.txt')
@@ -21,6 +26,24 @@ class WordSearchTests(unittest.TestCase):
             fileObject = open(testReader.filePath)
             inputText  = testReader.readText(fileObject)
             self.assertTrue(inputText)  # at this point i just want to check that there is text read - don't care if the input is valid
+        finally:
+            fileObject.close()
+
+    # -----------
+    # inputParser
+    # -----------
+
+    def testInputParserGrabsTextFromInputReader(self):
+        try:
+            testReader = inputReader('C://users/nschapka/desktop/input.txt')
+            testParser = inputParser()
+
+            fileObject = open(testReader.filePath)
+            inputText = testReader.readText(fileObject)
+            testParser.getTextToParse(testReader)
+
+            self.assertTrue(testParser.textToParse)
+
         finally:
             fileObject.close()
 
---
+++ inputParser.py
@@ -0,0 +1,7 @@
+class inputParser:
+
+    def __init__(self):
+        pass
+
+    def getTextToParse(self, inputReader):
+        self.textToParse = inputReader.inputText
